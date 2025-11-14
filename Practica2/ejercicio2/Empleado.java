93e98f6141f725dfb2b354b80c0d4f9470e643aa
Yareliz <Yarev15geminis@gmail.com>
2025-11-14 13:08:53 -0400

    commit

---
+++ Practica2/ejercicio2/Empleado.java
@@ -0,0 +1,34 @@
+package Practica2.ejercicio2;
+
+public class Empleado {
+    private String nombre;
+    private String cargo;
+    private double sueldo;
+
+    public Empleado(String nombre, String cargo, double sueldo){
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.sueldo = sueldo;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public double getSueldo(){
+        return sueldo;
+    }
+    public void setSueldo(double sueldo){
+        this.sueldo = sueldo;
+    }
+    @Override
+    public String toString(){
+        return " -> NOmbre; " + nombre + ", Cargo: " + cargo + ", sueldo: $" + String.format("%.2f", sueldo);
+    }
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Empleado empleado = (Empleado) obj;
+        return nombre.equals(empleado.nombre) && cargo.equals(empleado.cargo);
+    }
+    
+}
